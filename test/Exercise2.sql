50f65384e03a6a773dc1271e94508c9b2ad2d1f5
vladdan16 <v.danshov@innopolis.university>
2023-02-28 15:43:06 +0300

    ex2

--- test/Exercise2.sql
+++ test/Exercise2.sql
@@ -1,22 +1,27 @@
 -- Exercise 2
 
+DROP TABLE loan_books;
+DROP TABLE teacher_courses;
+DROP TABLE schools;
+DROP TABLE books;
+
 CREATE TABLE books (
     book_id SERIAL PRIMARY KEY,
-    book_title VARCHAR(50),
-    publisher VARCHAR(15)
+    book_title VARCHAR(80),
+    publisher VARCHAR(30)
 );
 
 CREATE TABLE schools (
     school_id SERIAL PRIMARY KEY,
-    school VARCHAR(50)
+    school VARCHAR(80)
 );
 
 CREATE TABLE teacher_courses (
     tc_id SERIAL PRIMARY KEY,
     teacher VARCHAR(30),
     course VARCHAR(40),
-    room VARCHAR(10),
-    grade VARCHAR(15),
+    room VARCHAR(30),
+    grade VARCHAR(30),
     loanDate DATE,
     school_id INTEGER REFERENCES schools(school_id)
 );
@@ -26,3 +31,26 @@ CREATE TABLE loan_books (
     tc_id INTEGER REFERENCES teacher_courses(tc_id),
     PRIMARY KEY (book_id, tc_id)
 );
+
+INSERT INTO schools (school)
+VALUES ('Horizon Education Institute'), ('Bright Institution');
+
+INSERT INTO books (book_title, publisher)
+VALUES ('Learning and teaching in early childhood education', 'BOA Editions'),
+       ('Preschool, N56', 'Taylor & Francis Publishing'),
+       ('Early Childhood Education N9', 'Prentice Hall'),
+       ('Know how to educate: guide for Parents and Teachers', 'McGraw Hill');
+
+INSERT INTO teacher_courses (teacher, course, room, grade, loanDate, school_id)
+VALUES ('Chad Russell', 'Logical Thinking', '1.A01', '1st grade', '2010-09-09', 1),
+       ('Chad Russell', 'Writing', '1.A01', '1st grade', '2010-05-05', 1),
+       ('Chad Russell', 'Numerical thinking', '1.A01', '1st grade', '2010-05-05', 1),
+       ('E.F.Codd', 'Spatial, Temporal and Causal Thinking', '1.B01', '1st grade', '2010-05-06', 1),
+       ('E.F.Codd', 'Numerical thinking', '1.B01', '1st grade', '2010-05-06', 1),
+       ('Jones Smith', 'Writing', '1.A01', '2nd grade', '2010-09-09', 1),
+       ('Jones Smith', 'English', '1.A01', '2nd grade', '2010-05-05', 1),
+       ('Adam Baker', 'Logical Thinking', '2.B01', '1st grade', '2010-12-18', 2),
+       ('Adam Baker', 'Numerical Thinking', '2.B01', '1st grade', '2010-05-06', 2);
+
+INSERT INTO loan_books (book_id, tc_id)
+VALUES (1, 1), (2, 2), (1, 3), (3, 4), (1, 5), (1, 6), (4, 7), (4, 8), (1, 9);
