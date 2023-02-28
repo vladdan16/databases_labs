cc856cc19c8af60d00bcf417553bb80333e8d4fa
vladdan16 <v.danshov@innopolis.university>
2023-02-28 13:10:58 +0300

    week6

--- test/Exercise1.sql
+++ test/Exercise1.sql
@@ -1,84 +1,106 @@
 -- Exercise 1
 
 CREATE TABLE orders
-	(orderId INT,
-	date DATE,
-	customerId INT,
-	customerName VARCHAR(15),
-	city VARCHAR(15),
-	itemId INT,
-	itemName VARCHAR(15),
-	quantity INT,
-	price REAL,
-	PRIMARY KEY (orderId, customerId, itemId)
-	);
+(
+    orderId      INT,
+    date         DATE,
+    customerId   INT,
+    customerName VARCHAR(15),
+    city         VARCHAR(15),
+    itemId       INT,
+    itemName     VARCHAR(15),
+    quantity     INT,
+    price        REAL,
+    PRIMARY KEY (orderId, customerId, itemId)
+);
 
-INSERT INTO orders VALUES ('2301', '2011-02-23', '101', 'Martin', 'Prague', '3786', 'Net', '3', '35.00');
-INSERT INTO orders VALUES ('2301', '2011-02-23', '101', 'Martin', 'Prague', '4011', 'Racket', '6', '65.00');
-INSERT INTO orders VALUES ('2301', '2011-02-23', '101', 'Martin', 'Prague', '9132', 'Pack-3', '8', '4.75');
-INSERT INTO orders VALUES ('2302', '2012-02-25', '107', 'Herman', 'Madrid', '5794', 'Pack-6', '4', '5.00');
-INSERT INTO orders VALUES ('2303', '2011-11-27', '110', 'Pedro', 'Moscow', '4011', 'Racket', '2', '65.00');
-INSERT INTO orders VALUES ('2303', '2011-11-27', '110', 'Pedro', 'Moscow', '3141', 'Cover', '2', '10.00');
+INSERT INTO orders
+VALUES ('2301', '2011-02-23', '101', 'Martin', 'Prague', '3786', 'Net', '3', '35.00');
+INSERT INTO orders
+VALUES ('2301', '2011-02-23', '101', 'Martin', 'Prague', '4011', 'Racket', '6', '65.00');
+INSERT INTO orders
+VALUES ('2301', '2011-02-23', '101', 'Martin', 'Prague', '9132', 'Pack-3', '8', '4.75');
+INSERT INTO orders
+VALUES ('2302', '2012-02-25', '107', 'Herman', 'Madrid', '5794', 'Pack-6', '4', '5.00');
+INSERT INTO orders
+VALUES ('2303', '2011-11-27', '110', 'Pedro', 'Moscow', '4011', 'Racket', '2', '65.00');
+INSERT INTO orders
+VALUES ('2303', '2011-11-27', '110', 'Pedro', 'Moscow', '3141', 'Cover', '2', '10.00');
 
 -- Exercise 1a
 
-CREATE TABLE cities (
-  cityId INT,
-  city VARCHAR(15),
-  PRIMARY KEY (cityId)
+CREATE TABLE cities
+(
+    cityId INT,
+    city   VARCHAR(15),
+    PRIMARY KEY (cityId)
 );
 
-CREATE TABLE items (
-  itemId INT,
-  itemName VARCHAR(15),
-  price REAL,
-  PRIMARY KEY (itemId)
+CREATE TABLE items
+(
+    itemId   INT,
+    itemName VARCHAR(15),
+    price    REAL,
+    PRIMARY KEY (itemId)
 );
 
-CREATE TABLE customers (
-  customerId INT,
-  customerName VARCHAR(15),
-  cityId INT,
-  PRIMARY KEY (customerId),
-  FOREIGN KEY (cityId) REFERENCES cities(cityId)
+CREATE TABLE customers
+(
+    customerId   INT,
+    customerName VARCHAR(15),
+    cityId       INT,
+    PRIMARY KEY (customerId),
+    FOREIGN KEY (cityId) REFERENCES cities (cityId)
 );
 
-CREATE TABLE orders (
-  orderId INT,
-  date DATE,
-  customerId INT,
-  itemId INT,
-  quantity INT,
-  PRIMARY KEY (orderId, customerId, itemId),
-  FOREIGN KEY (customerId) REFERENCES customers(customerId),
-  FOREIGN KEY (itemId) REFERENCES items(itemId)
+CREATE TABLE orders
+(
+    orderId    INT,
+    date       DATE,
+    customerId INT,
+    itemId     INT,
+    quantity   INT,
+    PRIMARY KEY (orderId, customerId, itemId),
+    FOREIGN KEY (customerId) REFERENCES customers (customerId),
+    FOREIGN KEY (itemId) REFERENCES items (itemId)
 );
 
 INSERT INTO cities (cityId, city)
-VALUES (1, 'Prague'), (2, 'Madrid'), (3, 'Moscow');
+VALUES (1, 'Prague'),
+       (2, 'Madrid'),
+       (3, 'Moscow');
 
 INSERT INTO customers (customerId, customerName, cityId)
-VALUES (101, 'Martin', 1), (107, 'Herman', 2), (110, 'Pedro', 3);
+VALUES (101, 'Martin', 1),
+       (107, 'Herman', 2),
+       (110, 'Pedro', 3);
 
 INSERT INTO items (itemId, itemName, price)
-VALUES (3786, 'Net', 35.00), (4011, 'Racket', 65.00), (9132, 'Pack-3', 4.75), (5794, 'Pack-6', 5.00), (3141, 'Cover', 10.00);
+VALUES (3786, 'Net', 35.00),
+       (4011, 'Racket', 65.00),
+       (9132, 'Pack-3', 4.75),
+       (5794, 'Pack-6', 5.00),
+       (3141, 'Cover', 10.00);
 
 INSERT INTO orders (orderId, date, customerId, itemId, quantity)
-VALUES (2301, '2011-02-23', 101, 3786, 3), (2301, '2011-02-23', 101, 4011, 6), (2301, '2011-02-23', 101, 9132, 8),
+VALUES (2301, '2011-02-23', 101, 3786, 3),
+       (2301, '2011-02-23', 101, 4011, 6),
+       (2301, '2011-02-23', 101, 9132, 8),
        (2302, '2012-02-25', 107, 5794, 4),
-       (2303, '2011-11-27', 110, 4011, 2), (2303, '2011-11-27', 110, 3141, 2);
+       (2303, '2011-11-27', 110, 4011, 2),
+       (2303, '2011-11-27', 110, 3141, 2);
 
 -- Exercise 1b
 
 SELECT o.orderId, SUM(o.quantity) as total_items, SUM(o.quantity * i.price) as total_amount
 FROM orders o
-JOIN items i ON o.itemId = i.itemId
+         JOIN items i ON o.itemId = i.itemId
 GROUP BY o.orderId;
 
 SELECT c.customerId, c.customerName, SUM(o.quantity * i.price) AS total_spent
 FROM customers c
-JOIN orders o ON c.customerId = o.customerId
-JOIN items i ON o.itemId = i.itemId
+         JOIN orders o ON c.customerId = o.customerId
+         JOIN items i ON o.itemId = i.itemId
 GROUP BY c.customerId, c.customerName
 ORDER BY total_spent DESC
 LIMIT 1;
--- week06/Exercise2.sql
+++ week06/Exercise2.sql
@@ -1,23 +1,28 @@
 -- Exercise 2
 
-CREATE TABLE loan_books
-	(school VARCHAR(50),
+CREATE TABLE books (
+    book_id SERIAL PRIMARY KEY,
+    book_title VARCHAR(50),
+    publisher VARCHAR(15)
+);
+
+CREATE TABLE schools (
+    school_id SERIAL PRIMARY KEY,
+    school VARCHAR(50)
+);
+
+CREATE TABLE teacher_courses (
+    tc_id SERIAL PRIMARY KEY,
     teacher VARCHAR(30),
     course VARCHAR(40),
     room VARCHAR(10),
     grade VARCHAR(15),
-    book VARCHAR(50),
-    publisher VARCHAR(15),
     loanDate DATE,
-	PRIMARY KEY (school, teacher, course, book, loanDate)
-	);
+    school_id INTEGER REFERENCES schools(school_id)
+);
 
-INSERT INTO loan_books VALUES ('Horizon Education Institute', 'Chad Russell', 'Logical Thinking', '1.A01', '1st grade', 'Learning and teaching in early childhood education', 'BOA Editions', 2010-09-09);
-INSERT INTO loan_books VALUES ('Horizon Education Institute', 'Chad Russell', 'Writing', '1.A01', '1st grade', 'Preschool, N56', 'Taylor & Francis Publishing', 2010-05-05);
-INSERT INTO loan_books VALUES ('Horizon Education Institute', 'Chad Russell', 'Numerical thinking', '1.A01', '1st grade', 'Learning and teaching in early childhood education', 'BOA Editions', 2010-05-05);
-INSERT INTO loan_books VALUES ('Horizon Education Institute', 'E.F.Codd', 'Spatial, Temporal and Causal Thinking', '1.B01', '1st grade', 'Early Childhood Education N9', 'Prentice Hall', 2010-05-06);
-INSERT INTO loan_books VALUES ('Horizon Education Institute', 'E.F.Codd', 'Numerical thinking', '1.B01', '1st grade', 'Learning and teaching in early childhood education', 'BOA Editions', 2010-05-06);
-INSERT INTO loan_books VALUES ('Horizon Education Institute', 'Jones Smith', 'Writing', '1.A01', '2nd grade', 'Learning and teaching in early childhood education', 'BOA Editions', 2010-09-09);
-INSERT INTO loan_books VALUES ('Horizon Education Institute', 'Jones Smith', 'English', '1.A01', '2nd grade', 'Know how to educate: guide for Parents and Teachers', 'McGraw Hill', 2010-05-05);
-INSERT INTO loan_books VALUES ('Bright Institution', 'Adam Baker', 'Logical Thinking', '2.B01', '1st grade', 'Know how to educate: guide for Parents and Teachers', 'McGraw Hill', 2010-12-18);
-INSERT INTO loan_books VALUES ('Bright Institution', 'Adam Baker', 'Numerical Thinking', '2.B01', '1st grade', 'Learning and teaching in early childhood education', 'BOA Editions', 2010-05-06);
+CREATE TABLE loan_books (
+    book_id INTEGER REFERENCES books(book_id),
+    tc_id INTEGER REFERENCES teacher_courses(tc_id),
+    PRIMARY KEY (book_id, tc_id)
+);
